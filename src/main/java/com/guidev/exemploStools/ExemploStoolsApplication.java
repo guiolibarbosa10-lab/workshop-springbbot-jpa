be7f50d9c7be20b02c6f835faf0f6a5df3092142
Gui Barbosa <guioli.barbosa10@gmail.com>
2026-06-25 14:48:12 -0300

    Pjt Created

---
+++ src/main/java/com/guidev/exemploStools/ExemploStoolsApplication.java
@@ -0,0 +1,13 @@
+package com.guidev.exemploStools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploStoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploStoolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guidev/exemploStools/ExemploStoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guidev.exemploStools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploStoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
